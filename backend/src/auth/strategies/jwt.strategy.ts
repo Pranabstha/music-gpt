3a11e802fbccc7ceee9276b73761f8584f4cdbc0
Pranabstha <pranabstha780@gmail.com>
2026-04-13 01:27:32 +0545

    changes

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -7,17 +7,6 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private prisma: PrismaService) {
-    const secret = process.env.JWT_REFRESH_SECRET;
-    console.log('🔑 secret length:', secret?.length);
-    const jwt = require('jsonwebtoken');
-    const testToken =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzN2UxOWM0NC04ZTRkLTQ4MGEtYjkyMC1jMDI5ZjNiOTRhZjkiLCJlbWFpbCI6InByYW5hYkBnbWFpbC5jb20iLCJpYXQiOjE3NzYwMTYwNDIsImV4cCI6MTc3NjYyMDg0Mn0.aEFv3htb5Z_DskK_PFw2V2nzys7WUNs7DDdahQTYbDk';
-    try {
-      const decoded = jwt.verify(testToken, secret);
-      console.log('✅ token valid:', decoded);
-    } catch (err) {
-      console.log('❌ token error:', err.message);
-    }
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET!,
